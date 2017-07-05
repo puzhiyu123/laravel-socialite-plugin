de584ba349247c10c9fadade10fbba63a8c94649
Zhiyu Pu <georgepu45@gmail.com>
2017-07-05 15:32:30 -0400

    added dashboard, logincontroller and authtication

---
+++ app/Exceptions/SocialAuthException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhiyupu
+ * Date: 2017-07-05
+ * Time: 3:31 PM
+ */
+
+namespace App\Exceptions;
+
+use Exception;
+
+class SocialAuthException extends Exception
+{
+
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhiyupu
+ * Date: 2017-07-05
+ * Time: 3:31 PM
+ */
+
+@extends('layouts.master')
+@section('page-title', 'Dashboard')
+@section('page-content')
+    <div class="col-md-10">
+        <h3>This is the dashboard for the sample app.</h3>
+    </div>
+@stop
