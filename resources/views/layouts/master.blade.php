5b3c2b6414f2196ea7485c0c25d67fe5a9fc19ac
Zhiyu Pu <georgepu45@gmail.com>
2017-07-05 15:22:51 -0400

    create new environment and add facebook plugin

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,26 +24,13 @@
 </style>
 </head>
 <body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">George Pu Social Login</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <a href="#">Sign In <i class="fa fa-sign-in"></i></a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<div id="navbar" class="collapse navbar-collapse">
+    <ul class="nav navbar-nav navbar-right">
+        <li>
+            <a href="{{ action('LoginController@showLoginPage') }}">Sign In <i class="fa fa-sign-in"></i></a>
+        </li>
+    </ul>
+</div>
 <div class="container">
     @yield('page-content')
 </div>
