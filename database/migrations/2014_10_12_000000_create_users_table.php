1e3b08559341216e4a69bf01f6cc9e8a045d964a
Zhiyu Pu <georgepu45@gmail.com>
2017-07-05 15:29:20 -0400

    established migrations and databases setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -33,3 +31,4 @@ class CreateUsersTable extends Migration
         Schema::dropIfExists('users');
     }
 }
+
